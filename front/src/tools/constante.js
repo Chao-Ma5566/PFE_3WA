2339bf3a248c77954b51da604831347a5ce8b765
Chao-Ma5566 <mjc1378098@gmail.com>
2024-02-01 08:49:54 +0100

    update: change db local with sql file

--- front/src/tools/constante.js
+++ front/src/tools/constante.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://jianchaoma.ide.3wa.io:3001'
-export const BASE_IMG = 'http://jianchaoma.ide.3wa.io:3001/img'
+export const BASE_URL = 'http://localhost:3001'
+export const BASE_IMG = 'http://localhost:3001/img'
